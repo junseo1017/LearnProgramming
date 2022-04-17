e942cd8cd77f37354f44a7cfba452913392919e6
junseolee <dlwnstjzld@gmail.com>
2022-04-17 21:33:57 +0900

    check

--- javascript/prototype,class.js
+++ javascript/prototype,class.js
@@ -1,27 +1,27 @@
 // class는 객체를 생성하기 위한 템플릿.
-// 기존에는 함수를 생성해서 객체를 반환하는 함수, 즉 factory 함수를 통해 객체를 생성했음. 
+// 기존에는 함수를 생성해서 객체를 반환하는 함수, 즉 factory 함수를 통해 객체를 생성했음.
 // 예시
-function CreateMonster(name,hp,att,xp){
-  return {name, hp, att, xp};
+function CreateMonster(name, hp, att, xp) {
+  return { name, hp, att, xp };
 }
-const monster1 = CreateMonster('슬라임',25,10,10);
-const monster2 = CreateMonster('오크',25,10,10);
+const monster1 = CreateMonster('슬라임', 25, 10, 10);
+const monster2 = CreateMonster('오크', 25, 10, 10);
 
-// 비슷한 예시로 new를 붙여 호출하는 함수를 생성자 함수 참고 
+// 비슷한 예시로 new를 붙여 호출하는 함수를 생성자 함수 참고
 // 생성자 함수는 대문자로 시작
-function Monster(name,hp,att,xp){
+function Monster(name, hp, att, xp) {
   this.name = name;
   this.hp = hp;
   this.att = att;
   this.xp = xp;
 }
-const monster1 = new Monster('슬라임',25,10,10);
-const monster2 = new Monster('오크',25,10,10);
+const monster1 = new Monster('슬라임', 25, 10, 10);
+const monster2 = new Monster('오크', 25, 10, 10);
 
-// 클래스를 적용한 함수는 아래와 같음 
+// 클래스를 적용한 함수는 아래와 같음
 // new를 써서 객체를 생성하지 않으면 오류가 남
-class Monster{
-  constructor(name,hp,att,xp){
+class Monster {
+  constructor(name, hp, att, xp) {
     this.name = name;
     this.hp = hp;
     this.att = att;
@@ -29,28 +29,27 @@ class Monster{
   }
 }
 
-const monster1 = new Monster('슬라임',25,10,10);
-const monster2 = new Monster('오크',25,10,10);
+const monster1 = new Monster('슬라임', 25, 10, 10);
+const monster2 = new Monster('오크', 25, 10, 10);
 
 // 객체 안에 메서드를 추가
-class Monster{
-  constructor(name,hp,att,xp){
+class Monster {
+  constructor(name, hp, att, xp) {
     this.name = name;
     this.hp = hp;
     this.att = att;
     this.xp = xp;
   }
-  attack(monster){
+  attack(monster) {
     monster.hp -= this.att;
     this.hp -= monster.att;
   }
 }
 
-
 // prototype
 // 프로토타입은 유전자라고 이해하면 편함.
 // 부모 함수
-function a(){
+function a() {
   this.name = 'kim';
   this.height = '170';
 }
@@ -58,9 +57,9 @@ function a(){
 const b = new a();
 
 // 부모 유전자에 새로운 속성을 부여
-a.prototype.hobby='movie';
+a.prototype.hobby = 'movie';
 
 // 자식 함수에 값을 직접 추가하지 않아도 자식 함수에서 속성이 추가됨
 // 하지만 부모 함수에 추가한게 아니라 부모 함수의 유전자에 추가한 것이기 때문에 부모 함수는 프로토타입으로 추가된 속성값을 가지지 않음.th
-console.log(b.hobby); // 'movie'  
+console.log(b.hobby); // 'movie'
 console.log(a.hobby); // undefined
---
+++ react/react.js
@@ -0,0 +1,47 @@
+// react는 데이터 바인딩(서버에서 데이터를 받아 HTML에 적용하는 것)이 쉽다는 장점이 있음.
+// 데이터 바인딩을 쉽게 할 수 있도록 도와줌
+// 전통적인 데이터 바인딩 // document.querySelect와 같은 방법을 사용했음
+//react의 경우 {변수명}를 입력하여 바로 적용 가능 / 함수도 사용 가능
+// {} 중괄호를 사용하여 변수, 함수, 클래스명, href, id, src 등 다양하게 사용 가능
+
+// style 적용
+// oject 자료형으로 만든 스타일을 작성해야함
+// font-size와 같은 기호는 사용하지 않고 camelCase 작명 방식을 사용
+function App() {
+  return <div style={{ color: "red", fontSize: "30px" }}>hi</div>;
+}
+// 보통은 직접 스타일을 작성하지 않고 변수명으로 묶어서 사용
+let post = { color: "red", fontSize: "30px" };
+function App() {
+  return <div style={post}>hi</div>;
+}
+
+// state
+// 데이터는 변수 또는 state에 넣어야 함.
+// state를 사용하는 이유 : 웹이 app처럼 동작하게 하려면 state에 데이터를 저장해놔야 함. > state는 변경되면 HTML이 자동으로 재렌더링이 됨.
+// 일반 변수는 변경되어도 재렌더링이 안됨.
+// 자주 바뀌는, 중요한 데이터는 변수 말고 state로 저장해야 함.
+
+// useState는 항상 2개인 배열이 생성됨.
+// 이를 활용해 구조할당분배를 적용하여 test라는 변수에 값을 저장하는 방식
+// useState 내에는 문자,숫자,배열,객체 모두 저장 가능.
+import React, { useState } from "react";
+function App() {
+  let [test, fixText] = useState("테스트");
+  let [test2, fixText2] = useState("테스트");
+
+  return <div>{test}</div>;
+}
+// 결과값. '테스트'
+
+// 이벤트리스너 달기
+// onClick이벤트
+// onClick 이후에는 중괄호를 넣고 함수가 들어가야 함
+function App() {
+  return <div onClick={() => {console.log(1)}}>{test}</div>;
+}
+
+// state 변경
+// 깊은 복사를 통해 새로운 변수에 저장하고 그 변수를 수정한다.
+// state 선언 시 생성됐던 배열의 두번째 요소에 저장한 변수를 적용한다.
+
