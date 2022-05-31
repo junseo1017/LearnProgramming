d7c7fd03b1414474d2a75216c8be79fcd4d0c749
junseolee <dlwnstjzld@gmail.com>
2022-06-01 08:36:26 +0900

    algorithm

--- algorithm/lev1 complete/lev1 2016년.js
+++ algorithm/lev1 complete/lev1 2016년.js
@@ -1,8 +1,8 @@
-// function solution(a, b) {
-//   const day = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
-//   const date = new Date(2016, a - 1, b).getDay();
-//   return day[date];
-// }
+function solution(a, b) {
+  const day = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+  const date = new Date(2016, a - 1, b).getDay();
+  return day[date];
+}
 
 function solution(a, b) {
   const {date} = new Date(2016, a - 1, b).getDay();
---
+++ algorithm/lev1 complete/lev1 최소직사각형.js
@@ -0,0 +1,26 @@
+function solution(sizes) {
+  let row = 0;
+  let column = 0;
+  sizes.map((e) => e.sort((a, b) => b - a));
+  for (let e of sizes) {
+    if (row <= e[0]) {
+      row = e[0];
+    }
+    if (column <= e[1]) {
+      column = e[1];
+    }
+  }
+  return row * column;
+
+  //   return answer;
+}
+solution([
+  [60, 50],
+  [30, 70],
+  [60, 30],
+  [80, 40],
+]);
+// 1	60	50
+// 2	30	70
+// 3	60	30
+// 4	80	40
